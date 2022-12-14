9d62fd2434ebaf44c2cfa800b0ce7dc8dd2a2a24
rayabarapunaresh <43800475+rayabarapunaresh@users.noreply.github.com>
2022-12-14 11:12:22 +0530

    Update Loadbalencers.tf

--- Loadbalencers.tf
+++ Loadbalencers.tf
@@ -5,7 +5,7 @@ resource "aws_lb" "ApplicationLoadBalancer" {
   name = "name1"
   load_balancer_type = "type1"
   internal = "internal1"
-  security_groups = "security1"
-  subnets  = "subnets123"
+  security_groups =  [ "sg_id" ]
+  subnets  =  [ "subnet_id" ]
 }
 
